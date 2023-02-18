8cb58ed1659a145c7658cf687086b66522eb7478
rodolfoSantosUK <rodolfo.gouveia2002@gmail.com>
2023-02-18 13:04:06 -0300

    Padrao de projeto abstract factory

---
+++ api-transaction/abstractFactory_Criacional/src/main/java/com/example/abstractfactory_criacional/AbstractFactoryCriacionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.abstractfactory_criacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractFactoryCriacionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbstractFactoryCriacionalApplication.class, args);
+    }
+
+}
---
+++ api-transaction/abstractFactory_Criacional/src/test/java/com/example/abstractfactory_criacional/AbstractFactoryCriacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abstractfactory_criacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractFactoryCriacionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
